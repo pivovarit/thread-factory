7fd44a0d1cbeba9bff423ae1a31cdc982d3ec449
pivovarit <gpiwowarek@gmail.com>
2019-01-26 15:10:49 +0100

    Add ThreadFactoryBuilder initial impl

---
+++ src/main/java/com/pivovarit/concurrent/ThreadFactoryBuilder.java
@@ -0,0 +1,85 @@
+package com.pivovarit.concurrent;
+
+import java.lang.Thread.UncaughtExceptionHandler;
+import java.util.Locale;
+import java.util.Objects;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicLong;
+
+public final class ThreadFactoryBuilder {
+    private String nameFormat = null;
+    private Boolean daemon = null;
+    private Integer priority = null;
+    private UncaughtExceptionHandler uncaughtExceptionHandler = null;
+    private ThreadFactory backingThreadFactory = null;
+
+    public ThreadFactoryBuilder() {
+    }
+
+    public ThreadFactoryBuilder setNameFormat(String nameFormat) {
+        this.nameFormat = nameFormat;
+        return this;
+    }
+
+    public ThreadFactoryBuilder setDaemon(boolean daemon) {
+        this.daemon = daemon;
+        return this;
+    }
+
+    public ThreadFactoryBuilder setPriority(int priority) {
+        if (priority <= 1 || priority >= 10) {
+            throw new IllegalArgumentException("Thread priority must be between 1 and 10");
+        }
+        this.priority = priority;
+        return this;
+    }
+
+    public ThreadFactoryBuilder setUncaughtExceptionHandler(UncaughtExceptionHandler uncaughtExceptionHandler) {
+        this.uncaughtExceptionHandler = Objects.requireNonNull(uncaughtExceptionHandler);
+        return this;
+    }
+
+    public ThreadFactoryBuilder setThreadFactory(ThreadFactory backingThreadFactory) {
+        this.backingThreadFactory = Objects.requireNonNull((backingThreadFactory);
+        return this;
+    }
+
+    public ThreadFactory build() {
+        return build(this);
+    }
+
+    private static ThreadFactory build(ThreadFactoryBuilder builder) {
+        final String nameFormat = builder.nameFormat;
+        final Boolean daemon = builder.daemon;
+        final Integer priority = builder.priority;
+        final UncaughtExceptionHandler uncaughtExceptionHandler = builder.uncaughtExceptionHandler;
+        final ThreadFactory backingThreadFactory = builder.backingThreadFactory != null ? builder.backingThreadFactory : Executors.defaultThreadFactory();
+        final AtomicLong count = nameFormat != null ? new AtomicLong(0L) : null;
+        return runnable -> {
+            Thread thread = backingThreadFactory.newThread(runnable);
+            if (nameFormat != null) {
+                thread.setName(ThreadFactoryBuilder.format(nameFormat, count.getAndIncrement()));
+            }
+
+            if (daemon != null) {
+                thread.setDaemon(daemon);
+            }
+
+            if (priority != null) {
+                thread.setPriority(priority);
+            }
+
+            if (uncaughtExceptionHandler != null) {
+                thread.setUncaughtExceptionHandler(uncaughtExceptionHandler);
+            }
+
+            return thread;
+        };
+    }
+
+    private static String format(String format, Object... args) {
+        return String.format(Locale.ROOT, format, args);
+    }
+}
+
