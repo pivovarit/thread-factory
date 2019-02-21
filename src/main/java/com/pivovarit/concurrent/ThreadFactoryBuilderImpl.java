2f025418699fe5380aa1e36aa64a89cafc26c9f4
pivovarit <gpiwowarek@gmail.com>
2019-02-21 11:04:43 +0100

    Use AtomicInteger

--- src/main/java/com/pivovarit/concurrent/ThreadFactoryBuilderImpl.java
+++ src/main/java/com/pivovarit/concurrent/ThreadFactoryBuilderImpl.java
@@ -6,7 +6,6 @@ import java.util.Objects;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * @author Grzegorz Piwowarek
